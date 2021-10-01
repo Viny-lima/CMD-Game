ef9c44b1d45d64bb49cdef5f8587085f9a4ff9ab
Viny-lima <viniciuslima.vf4@hotmail.com>
2021-10-01 11:07:40 -0300

    Adicionar arquivos de projeto.

---
+++ CMD-Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMD_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
